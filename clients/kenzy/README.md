e6eeddc098d28cfec24d121078f86a39fdd462e3
Sebastian <sebastian.melbye@gmail.com>
2021-02-22 21:52:23 +0100

    :rocket:[kenzy] new files created!

---
+++ clients/kenzy/README.md
@@ -0,0 +1,7 @@
+
+# CDN for kenzy
+
+### Sist oppdatert 
+Mon Feb 22 2021 21:51:19 GMT+0100 (sentraleuropeisk normaltid)
+### Laget 
+Sebastian
---
+++ clients/kenzy/kenzy.js
@@ -0,0 +1,4 @@
+
+        import { render } from "react-dom";
+        import Hello from "../../../src/nudges/Hello";
+        render(<Hello />, document.getElementById("otello-root"));
