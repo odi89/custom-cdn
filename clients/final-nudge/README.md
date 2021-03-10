d0c3ebb31e825ab6f7f3c3784846003a7c7f8e12
Sebastian <sebastian.melbye@gmail.com>
2021-03-10 03:52:21 +0100

    :rocket:[final-nudge] new files created!

---
+++ clients/final-nudge/README.md
@@ -0,0 +1,7 @@
+
+# CDN for final-nudge
+
+### Sist oppdatert 
+Wed Mar 10 2021 03:38:57 GMT+0100 (sentraleuropeisk normaltid)
+### Laget 
+Sebastian
---
+++ clients/final-nudge/final-nudge.js
@@ -0,0 +1,4 @@
+
+        import { hydrate } from "react-dom";
+        import Flisekompaniet from "../../../src/nudges/Flisekompaniet";
+        hydrate(<Flisekompaniet />, document.getElementById("otello-root"));
